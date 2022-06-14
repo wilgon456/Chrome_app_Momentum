fd6dafe96bea671a40431477b92d620184158bba
wilgon456 <juncatz1945@gmail.com>
2022-06-14 15:35:55 +0900

    220614_todolist_edit

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDolist = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos"
+
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_KEY,JSON.stringify(toDos))
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +31,16 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is turn of", item));
+}
