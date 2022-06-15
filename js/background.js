d34ed101449c498e7818d31893e0362a169d77fe
wilgon456 <54878785+wilgon456@users.noreply.github.com>
2022-06-16 00:36:46 +0900

    220616_jspart_finish

--- js/background.js
+++ js/background.js
@@ -1,13 +1,11 @@
-const images = [
-    "0.jpeg",
-    "1.jpeg",
-    "2.jpeg"
-]
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+bgImage.id = "bgImage";
